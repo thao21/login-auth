6f25f79951a894f77c1cc2e04afa52146595244a
thao21 <81501056+thao21@users.noreply.github.com>
2021-06-28 18:30:15 +0700

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack')
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jquery: 'jquery',
+        'window.jQuery': 'jquery',
+        jQuery: 'jquery'
+      })
+    ],
+    externals: {
+      jquery: 'jQuery',
+    }
+  }
+}
